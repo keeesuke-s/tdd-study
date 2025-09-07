f176113ca0a3bce677583a196b5ade51fabb3086
keeesuke-s <kei66.s0313@keio.jp>
2025-09-07 15:38:18 +0900

    Add tdd-java directory

---
+++ tdd-java/src/main/java/org/example/money/Money.java
@@ -0,0 +1,33 @@
+package org.example.money;
+
+class Money {
+    protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    // factory methods
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    String currency() {
+        return this.currency;
+    }
+
+    Money times(int multiplier) {
+        return new Money(this.amount * multiplier,currency);
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return this.amount == money.amount
+                && this.currency.equals(money.currency);
+    }
+}
